d8514db1fb64a554f88ef728b37ddf170a714133
HDeveloper <omur48666@gmail.com>
2026-04-30 23:01:52 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://workers.dev', 
+    bare: 'https://student-unblocker.com', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
