93e5253c5354a2904a8327a78f6bf48c3aa95ea9
HDeveloper <omur48666@gmail.com>
2026-04-30 22:55:59 -0400

    Add encodeUrl and decodeUrl to uv.config.js
    Add URL encoding and decoding functions to configuration

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,8 @@
 self.__uv$config = {
     prefix: '/uv/service/',
     bare: 'https://student-unblocker.com', 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
