6b34de4ad103edb9ea4b45c7eea0bb7586486058
HDeveloper <omur48666@gmail.com>
2026-04-30 22:57:54 -0400

    Fix config prefix comment in uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/uv/service/', // This must match!
     bare: 'https://student-unblocker.com', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
