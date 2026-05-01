4d7a668acb2ad7598d7384e1acfa05e6a2b9d826
HDeveloper <omur48666@gmail.com>
2026-04-30 23:01:09 -0400

    Change bare URL in uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://student-unblocker.com', 
+    bare: 'https://workers.dev', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
